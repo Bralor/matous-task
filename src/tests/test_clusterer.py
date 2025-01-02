3e9360cab79d9ee4c2b2eba16285a198761b095e
Matouš Holinka <bralor92@email.cz>
2025-01-02 20:33:46 +0100

    test_clusterer.py: Added the fixtures.

--- src/tests/test_clusterer.py
+++ src/tests/test_clusterer.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+import numpy
 import pytest
 
 from src.clusterer.clusterer import main
@@ -7,6 +10,23 @@ def test_dummy():
     assert main() == 1
 
 
-def test_reverse_dummy():
-    with pytest.raises(AssertionError):
-        assert main() == 0
+@pytest.fixture
+def testing_sample() -> numpy.ndarray:
+    """
+    Returns the two-dimensional array as a testing sample.
+
+    :return: 2D array,
+    :rtype: numpy.ndarray
+    """
+    return numpy.array([[1, 2], [2, 3], [1, 1], [8, 8], [9, 9], [10, 10]])
+
+
+@pytest.fixture
+def testing_config() -> Dict[str, int]:
+    """
+    Returns the object with the boundary conditions.
+
+    :return: The initial setup,
+    :rtype: Dict[str, int].
+    """
+    return {"n_clusters": 3, "n_init": 10, "max_iter": 300, "random_state": 42}
