3822c1a9ed945e79a3db1fc1825252818989d85e
Matouš Holinka <bralor92@email.cz>
2025-01-23 21:54:09 +0100

    containers.py: Added a reader providers.

--- src/clusterer/containers.py
+++ src/clusterer/containers.py
@@ -1,15 +1,51 @@
+import numpy.typing as npt
 from dependency_injector import containers, providers
+from dependency_injector.wiring import Provide, inject
 
-from clusterer.clusterer import DataLoader
+from clusterer.clusterer import DataLoader, KMeansProcessor
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
 
-    read_json = providers.Factory(DataLoader.from_json,
+    kmeans = providers.Factory(
+            KMeansProcessor,
+            n_clusters=config.algorithm.hyperparameters.n_clusters,
+            random_state=config.algorithm.hyperparameters.random_state,
+            max_iter=config.algorithm.hyperparameters.max_iter
+    )
+
+    # TODO: added more clustering algorithms
+
+    algorithm = providers.Selector(config.algorithm.name,
+                                   kmeans=kmeans)
+    # Source:
+    # https://python-dependency-injector.ets-labs.org/
+    # tutorials/cli.html#selector
+
+    read_json = providers.Factory(lambda source: DataLoader
+                                  .from_json(source)
+                                  .data,
                                   source=config.input.filepath)
 
-    read_numpy = providers.Factory(DataLoader.from_numpy,
+    read_numpy = providers.Factory(lambda source: DataLoader
+                                   .from_numpy(source)
+                                   .data,
                                    source=config.input.filepath)
 
-    # TODO: Add algorithms factories
+    read_source = providers.Selector(config.input.format,
+                                     json=read_json,
+                                     npy=read_numpy)
+
+    # TODO: Save the results
+
+
+@inject
+def create_clusters(source: npt.NDArray,
+                    algorithm=Provide[Container.algorithm]
+                    ) -> npt.NDArray[int]:
+    """
+    Run the clustering algorithm processor on the provided data.
+    """
+    model = algorithm
+    return model.fit(source)
